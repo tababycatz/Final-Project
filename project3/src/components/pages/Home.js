75702cb760150e74817118ac8c66f74b2dc750c7
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-11-07 21:06:52 -0500

    added routes and passport stuff

---
+++ project3/src/components/pages/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function Home() {
+  return (
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          Required
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+        <Form.Text className="text-muted">
+          Required
+        </Form.Text>
+      </Form.Group>
+      <Button variant="secondary" type="submit">
+        Submit
+      </Button>
+      <Button variant="secondary" type="submit">
+        Sign Up
+      </Button>
+    </Form>
+  );
+}
+
+export default Home;
