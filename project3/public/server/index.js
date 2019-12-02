2126d95ae836c5c4deea974a61aec9d86e66203e
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-12-02 10:50:41 -0500

    updating routes again

--- project3/public/server/index.js
+++ project3/public/server/index.js
@@ -1,9 +1,15 @@
- const express = require('express');
- const cors = require('cors');
- const passport = require('passport');
- const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
- const FacebookStrategy = require('passport-facebook').Strategy;
- const keys = require('../../config/');
+const express = require('express');
+const cors = require('cors');
+const path = require("path");
+const fs = require("fs");
+const http = require('http');
+const https = require('https');
+const passport = require('passport');
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const keys = require('../../config/');
+const chalk = require("chalk");
+
  let user = {};
 
  //Facebook Strategy//
@@ -17,58 +23,81 @@ passport.deserializeUser(function(user, cb) {
 
 passport.use(new FacebookStrategy({
     clientID: keys.FACEBOOK.clientID,
-    clientSecret : keys.FACEBOOK.clientSecret,
-    callbackURL: '/auth/facebook/callback'
+    clientSecret: keys.FACEBOOK.clientSecret,
+    callbackURL: "/auth/facebook/callback"
 },
-    (accessToken, refreshToken, profile, cb) => {
-        user = {...profile};
-        return cb(null,profile);
+(accessToken, refreshToken, profile, cb) => {
+    console.log(chalk.blue(JSON.stringify(profile)));
+    user = { ...profile };
+    return cb(null, profile);
+}));
 
-    }
-));
+passport.use(new GoogleStrategy({
+    clientID: keys.GOOGLE.clientID,
+    clientSecret: keys.GOOGLE.clientSecret,
+    callbackURL: "/auth/google/callback"
+},
+(accessToken, refreshToken, profile, cb) => {
+    console.log(chalk.blue(JSON.stringify(profile)));
+    user = { ...profile };
+    return cb(null, profile);
+}));
 
 const app = express();
 app.use(cors());
 app.use(passport.initialize());
 
-app.get('/auth/facebook', passport.authenticate('facebook'));
-app.get('/auth/facebook/callback',
-passport.authenticate('facebook', 
-    { successRedirect: '/profile',
-    failureRedirect: '/' }));
+app.get("/auth/facebook", passport.authenticate("facebook"));
+app.get("/auth/facebook/callback",
+    passport.authenticate("facebook"),
+    (req, res) => {
+        res.redirect("/profile");
+    });
 
-passport.use(new GoogleStrategy({
-    clientID: keys.GOOGLE.clientID,
-    clientSecret : keys.GOOGLE.clientSecret,
-    callbackURL: '/auth/google/callback'
-},
-    (accessToken, refreshToken, profile, cb) => {
-        user = {...profile};
-        return cb(null,profile);
-
-    }
-));
-
-app.get('/auth/google', passport.authenticate('google',{
-    scope: ['profile', 'email']
+app.get("/auth/google", passport.authenticate("google", {
+    scope: ["profile", "email"]
 }));
-app.get('/auth/google/callback',
-passport.authenticate('google', 
-    { successRedirect: '/profile',
-    failureRedirect: '/' }));
+app.get("/auth/google/callback",
+    passport.authenticate("google"),
+        (req, res) => {
+            res.redirect("/profile");
+        });
 
-app.get('/user', (req,res) => {
-    console.log('getting user data');
+app.get("/user", (req, res) => {
+    console.log("getting user data!");
     res.send(user);
 });
 
-app.get('/auth/logout', (req, res) => {
-    console.log('logging out');
+app.get("/auth/logout", (req, res) => {
+    console.log("logging out!");
     user = {};
-    res.redirect('/');
-})
+    res.redirect("/");
+});
     
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+if (process.env.NODE_ENV === "production") {
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/learnpassportjs.com/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/learnpassportjs.com/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/learnpassportjs.com/chain.pem', 'utf8');
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
 
+    https.createServer(credentials, app).listen(443, () => {
+        console.log('HTTPS Server running on port 443');
+    });
+    http.createServer(function (req, res) {
+        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+        res.end();
+    }).listen(80);
+} else if (process.env.NODE_ENV === "development") {
+    app.listen(5000);
+}
