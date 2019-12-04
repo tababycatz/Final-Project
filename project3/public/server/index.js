01477baac7e922dee7cd7887cd3ad6b23cecad7f
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-12-03 20:20:16 -0500

    updating authentication

--- project3/public/server/index.js
+++ project3/public/server/index.js
@@ -27,16 +27,6 @@ passport.use(new FacebookStrategy({
     }
 ));
 
-const app = express();
-app.use(cors());
-app.use(passport.initialize());
-
-app.get('/auth/facebook', passport.authenticate('facebook'));
-app.get('/auth/facebook/callback',
-passport.authenticate('facebook', 
-    { successRedirect: '/profile',
-    failureRedirect: '/' }));
-
 passport.use(new GoogleStrategy({
     clientID: keys.GOOGLE.clientID,
     clientSecret : keys.GOOGLE.clientSecret,
@@ -48,6 +38,18 @@ passport.use(new GoogleStrategy({
 
     }
 ));
+
+const app = express();
+app.use(cors());
+app.use(passport.initialize());
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook', 
+    { successRedirect: '/profile',
+    failureRedirect: '/' }));
+
+
 
 app.get('/auth/google', passport.authenticate('google',{
     scope: ['profile', 'email']
@@ -69,6 +71,6 @@ app.get('/auth/logout', (req, res) => {
 })
     
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
--- project3/src/setupProxy.js
+++ project3/src/setupProxy.js
@@ -1,5 +1,4 @@
-const proxy = require("http-proxy-middleware");
-
+const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
-    app.use(proxy('/auth', { target: 'http://localhost:3000/' }));
+app.use(proxy('/auth/google', { target: 'https://localhost:5000/' }));
 };
