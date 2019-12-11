6ab8aa0657c6fcdc0a4b9f786fffa027925b2b45
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-12-11 12:21:53 -0500

    cleaned some files

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from 'react';
 import './App.css';
 import Routes from './Routes';
 import NavBar from './components/navbar/NavBar';
-import withFirebaseAuth from 'react-with-firebase-auth'
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import firebaseConfig from './firebaseConfig';
 
 const App = () => (
   <div className="App">
